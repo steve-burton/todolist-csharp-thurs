7cd5f41b120d376684046dc8ec82aac4cd2c8cb5
Erik Killops and Levi Bibo <erik+levi@>
2016-11-16 09:28:47 -0800

    update paths to RESTful conventions.

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,6 +1,7 @@
 using Nancy;
 using ToDoList.Objects;
 using System.Collections.Generic;
+using System;
 
 namespace ToDoList
 {
@@ -8,20 +9,29 @@ namespace ToDoList
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["add_new_task.cshtml"];
-      Get["/view_all_tasks"] = _ => {
-        List<string> allTasks = Task.GetAll();
-        return View["view_all_tasks.cshtml", allTasks];
+      Get["/"] = _ => {
+        return View["index.cshtml"];
       };
-      Post["/task_added"] = _ => {
+      Get["/tasks"] = _ => {
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
+      };
+      Get["/tasks/new"] = _ => {
+        return View["task_form.cshtml"];
+      };
+      Post["/tasks"] = _ => {
         Task newTask = new Task (Request.Form["new-task"]);
-        newTask.Save();
-        return View["task_added.cshtml", newTask];
+        List<Task> allTasks = Task.GetAll();
+        return View["tasks.cshtml", allTasks];
       };
-      Post["/tasks_cleared"] = _ => {
-        Task.ClearAll();
-        return View["tasks_cleared.cshtml"];
+      Get["/tasks/{id}"] = parameters => {
+        Task task = Task.Find(parameters.id);
+        return View["task.cshtml", task];
       };
+      // Post["/tasks_cleared"] = _ => {
+      //   Task.ClearAll();
+      //   return View["tasks_cleared.cshtml"];
+      // };
     }
   }
 }
